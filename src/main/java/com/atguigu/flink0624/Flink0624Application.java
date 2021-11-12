78578c636f7aa94d270a8c568779b747f88d7263
hapi <hapi@163.com>
2021-11-12 15:24:05 +0800

    Initial commit

---
+++ src/main/java/com/atguigu/flink0624/Flink0624Application.java
@@ -0,0 +1,13 @@
+package com.atguigu.flink0624;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Flink0624Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Flink0624Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/atguigu/flink0624/Flink0624ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.flink0624;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Flink0624ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
